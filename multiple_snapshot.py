5108e57a6f2e926b35ef6dfea4c1f358e5776d64
Amin Beirami <beirami.m.a@gmail.com>
2018-02-11 02:41:44 -0500

    multiple snapshot visualization added

--- multiple_snapshot.py
+++ multiple_snapshot.py
@@ -2,15 +2,15 @@ import numpy as np
 import matplotlib.pyplot as pyplot
 import math
 from time import time
-SNAPSHOT_NO = 3
+SNAPSHOT_NO = 2
 QUERY_SIZE = 0
 cost_table_dyn = []
-time_cost_dyn = []
-time_cost_rec = []
+
 def make_queries():
     Q = np.vstack([
         np.random.normal(4, 4, (20, 1)),
-        np.random.normal(40, 4, (20, 1))
+        np.random.normal(40, 4, (20, 1)),
+        np.random.normal(20, 4, (20, 1))
         ])
     a, b = np.min(Q), np.max(Q)
     Q = np.vstack([Q, np.random.uniform(a, b, (10, 1))])
@@ -22,14 +22,21 @@ def timeit(f, *args):
     f(*args)
     duration = time() - start
     return duration
-def cost_array_single(Q):
-    snap_pos = np.median(Q)
-    f = open("data/single_snap.txt","a+")
+
+def save_file_queries(Q):
+    f = open("data/queries.txt","a+")
     for i in range(len(Q)):
-        cost = abs(Q[i]-snap_pos)
-        f.write(str(cost)+"\n")
+        f.write(str(Q[i])+"\n")
     f.close()
 
+def cost_array(Q,name):
+    cost = []
+    snap_pos = np.median(Q)
+    f = open("data/"+name+"_snap.txt","a+")
+    for i in range(len(Q)):
+        cost.append(abs(Q[i]-snap_pos))
+    f.write(str(cost) + "|" + str(snap_pos) + "|" + str(max(Q)) + "|" + str(min(Q)) + "\n")
+    f.close()
 
 def calc_single_cost(Q):
     if Q == []:
@@ -40,12 +47,6 @@ def calc_single_cost(Q):
         for i in range(len(Q)):
             cost += abs(Q[i] - snap_pos)
         return cost
-
-def save_file_queries(Q):
-    f = open("data/queries.txt","a+")
-    for i in range(len(Q)):
-        f.write(str(Q[i])+"\n")
-    f.close()
 
 def calc_cost(Q,snap_no,cut):
     if snap_no > 0:
@@ -69,16 +70,43 @@ def init_first_row(Q,size_queries):
         queries = Q[-i:]
         cost_table_dyn[1][i] = calc_single_cost(queries)
 
+def slice_the_queries(Q,cut):
+    query_slices = []
+    for i in range(len(cut)):
+        slices = Q[:cut[i]]
+        query_slices.append(slices)
+        Q = Q[cut[i]:]
+    return query_slices
+def the_best_cuts(Q,cut):
+    multiple_cuts = []
+    size = len(Q)
+    # cut.reverse()
+    cut.append(0)
+    for i in range(SNAPSHOT_NO):
+        slices = size - cut[i]
+        size = cut[i] 
+        multiple_cuts.append(slices)
+    return multiple_cuts
+
 def dynamic_cost(Q,no_snap,size_queries):
+    best_cut = []
     init_first_row(Q,size_queries)
     for j in range(2,no_snap+1):
         for i in range (1, size_queries+1):
             min_value = np.inf
+            index = 0
+            cut = 0
             for k in range(1,i+1):
-                min_value = min(min_value,cost_table_dyn[j-1][k]+calc_cost(Q[-i:],j-1,(i-k)))
+                current_cost = cost_table_dyn[j-1][k]+calc_cost(Q[-i:],j-1,(i-k))
+                if current_cost < min_value:
+                    min_value = current_cost
+                    index = j-1
+                    cut = i-k
                 # print "cost_table_dyn["+str(j-1)+"]["+str(k)+"]"+ "+calc_cost(Q[:"+str(i)+"],"+str(j-1)+","+str((i-k))+"))"
             # print "-----------------------------------"            
             cost_table_dyn[j][i] = min_value
+        best_cut.append(cut)
+    return best_cut
 
 def recursive_cost(Q,no_snap):
     if (no_snap == 1) or (len(Q) == 1):
@@ -94,10 +122,15 @@ save_file_queries(query_list)
 QUERY_SIZE = len(query_list)
 cost_table_dyn = np.zeros((SNAPSHOT_NO+1,QUERY_SIZE+1))
 init_cost_tables(SNAPSHOT_NO,QUERY_SIZE)
-dynamic_cost(query_list,SNAPSHOT_NO,QUERY_SIZE)
-cost_array_single(query_list)
+best_cut = dynamic_cost(query_list,SNAPSHOT_NO,QUERY_SIZE)
+cost_array(query_list,'single')
 print recursive_cost(query_list,SNAPSHOT_NO)
-print "---------------------------"
-print cost_table_dyn
+print best_cut
+multiple_cuts = the_best_cuts(query_list,best_cut)
+query_slices = slice_the_queries(query_list,multiple_cuts)
+for i in range (len(query_slices)):
+    cost_array(query_slices[i],'multiple')
+# cost_line = 
+# print cost_table_dyn
 # for i in range(1,SNAPSHOT_NO+1):
 #     print "when there is/are "+ str(i)+" snapshots, the minimum possible cost is: "+str(cost_table_dyn[i][QUERY_SIZE])
--- visualize.py
+++ visualize.py
@@ -1,11 +1,10 @@
 import matplotlib.pyplot as pl
 import numpy as np
-rec_no = []
-time_cost_dyn = []
-time_cost_rec = []
-query_list = []
-single_snap_cost = []
+
 def dyn_vs_rec():
+	rec_no = []
+	time_cost_dyn = []
+	time_cost_rec = []
 	with open ("data/dynamic.txt") as f:
 		for line in f:
 			fields = line.split(',')
@@ -29,25 +28,63 @@ def dyn_vs_rec():
 	# pl.legend()
 	pl.savefig('graphs/timecost.png', bbox_inches='tight')
 
-def single_snap():
+def queries():
+	query_list = []
 	with open("data/queries.txt") as f:
 		for line in f:
 			query_list.append(line)
 	f.close()
-	Q = np.array(query_list)
-	with open ("data/single_snap.txt") as p:
-		for line in p:
-			single_snap_cost.append(line)
-	p.close()
+	return query_list
+
+def snap_cost(name):
+	snap_cost = []
+	snap_pos = []
+	maximum = []
+	minimum = []
+	with open("data/"+name+"_snap.txt") as f:
+		for line in f:
+			sections = line.split('|')
+			costs = sections[0]
+			costs = costs.translate(None,'([])')
+			snap_cost.append(costs.split(","))
+			snap_pos.append(sections[1])
+			maximum.append(sections[2])
+			minimum.append(sections[3])
+	f.close()
+	return snap_cost, snap_pos, maximum, minimum
+
+def plot_single_cost():
+	single_snap_cost, snap_pos, min_val, max_val = snap_cost('single')
+	Q = np.array(queries())
 	pl.figure(facecolor = 'white')
 	pl.ylim([-10,50])
 	pl.plot(Q,np.zeros(Q.shape),'k*',label = 'query positions')
-	pl.plot(query_list,single_snap_cost,'k--',label = 'cost line' )
+	pl.plot(Q,single_snap_cost[0],'k--',label = 'cost line' )
+	pl.plot(snap_pos[0],0,'o',color = 'red', label = 'snapshot position')
 	pl.xlabel('timeline')
 	pl.ylabel('cost(unit?)')
 	legend = pl.legend(loc='upper left')
 	pl.savefig("graphs/single_snap_cost.png",bbox_inches = 'tight')
+
+def plot_multiple_cost():
+	complete_cost =[]
+	multi_snap_cost, snap_pos, min_val, max_val = snap_cost('multiple')
+	for i in range(len(multi_snap_cost)):
+		complete_cost = complete_cost+multi_snap_cost[i]
+	Q = np.array(queries())
+	snap_pos = np.array(snap_pos)
+	pl.figure(facecolor = 'white')
+	pl.ylim([-10,50])
+	pl.plot(Q,np.zeros(Q.shape),'k*',label = 'query positions')
+	pl.plot(Q,complete_cost,'k--',label = 'cost line' )
+	pl.plot(snap_pos,np.zeros(snap_pos.shape),"o",label = 'snapshot position',color = 'red')
+	pl.xlabel('timeline')
+	pl.ylabel('cost(unit?)')
+	legend = pl.legend(loc='upper left')
+	pl.savefig("graphs/multi_snap_cost.png",bbox_inches = 'tight')
+
 print 'visualizing....'
 dyn_vs_rec()
-single_snap()
+plot_single_cost()
+plot_multiple_cost()
 print 'visualization completed successfully.'
